a38705f116206868a15aed4b2625f84261c663ce
alexisc7 <alexislz.k9@gmail.com>
2025-03-29 20:16:49 -0300

    conexion db

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 
 app = Flask(__name__)
 
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect("usersdb.db")
+        g.cursor = g.db.cursor()
+    return g.cursor
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
+
 @app.route('/')
 def index():
+    cursor = get_db()
+    cursor.execute("SELECT 1 + 3")
+    result = cursor.fetchone()
+    print(result)
     return 'prueba'
 
 
